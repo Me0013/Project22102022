a24c6aaf247aaf1cb7a00d9c6263c7c9d4148183
Katerina Stredie <stredie_katerina@yahoo.com>
2022-10-17 19:30:57 +0300

    try

---
+++ src/main/java/ro/katy/tutorial/Boolean.java
@@ -0,0 +1,22 @@
+package ro.katy.tutorial;
+
+public class Boolean {
+	
+	public static void main(String[] args) {
+		
+		int a = 10, b = 30; 
+		
+		boolean check1 = a == b; 
+		
+		if (check1 == true) {
+			System.out.println(" a is equal to b.");
+		} else if (a > b) {
+			System.out.println(" a is bigger then b."); 
+		} else {
+			System.out.println("a is smaller then b.");
+			
+		}
+	
+	}
+
+}
